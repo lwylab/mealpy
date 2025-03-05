ef863eefb42d967f9ad9062ad0bf36b6e11cd037
梁文钰 <wenyu.liang@haiziwang.com>
2025-03-05 23:18:49 +0800

    测试代码

--- examples/run_simple_function.py
+++ examples/run_simple_function.py
@@ -3,23 +3,29 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
-
+# from numpy.distutils.system_info import f2py_info
 from opfunu.cec_based.cec2017 import F52017
+from opfunu.name_based.q_func import Quartic
 from mealpy import FloatVar, BBO
+from mealpy.swarm_based import HHO
 
 ## Define your own problems
 f1 = F52017(30, f_bias=0)
+f2 = Quartic(30)
 
 p1 = {
     "bounds": FloatVar(lb=f1.lb, ub=f1.ub),
-    "obj_func": f1.evaluate,
+    "obj_func": f2.evaluate,
     "minmax": "min",
     "name": "F5",
     "log_to": "console",
     "save_population": True
 }
 
-optimizer = BBO.OriginalBBO(epoch=100, pop_size=30)
+# optimizer = BBO.OriginalBBO(epoch=100, pop_size=30)
+# optimizer.solve(p1, seed=10)        # Set seed for each solved problem
+
+optimizer = HHO.OriginalHHO(epoch=100, pop_size=30)
 optimizer.solve(p1, seed=10)        # Set seed for each solved problem
 
 optimizer.history.save_diversity_chart()
